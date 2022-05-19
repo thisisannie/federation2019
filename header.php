f977c3e7413c12772cc9cebb7f098a8800c1095a
Annie Kyles <anniek@federation.net.nz>
2022-05-19 16:16:45 +1200

    Improved head indentation

--- header.php
+++ header.php
@@ -1,70 +1,56 @@
 <!doctype html>
 <html class="no-js" lang="">
-
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Federation | Leading change</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <link rel="manifest" href="site.webmanifest">
-  
-  <link rel="shortcut icon" type="image/x-icon" href="/favicon.png"/>
-
-   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500&family=Raleway:wght@100;300;400;500;700&display=swap" rel="stylesheet"> 
-
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/bootstrap-grid.css">
-  
-  <link rel="stylesheet" type="text/css" href="css/slick.css"/>
-  <link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
-  
-  <link rel="stylesheet" href="css/style.css?v=29">
-  
- 
- <script src="js/pace.min.js"></script>
-  
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Federation | Leading change</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="manifest" href="site.webmanifest">
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.png"/>   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500&family=Raleway:wght@100;300;400;500;700&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/bootstrap-grid.css">
+	<link rel="stylesheet" type="text/css" href="css/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
+	<link rel="stylesheet" href="css/style.css?v=29">
+	<script src="js/pace.min.js"></script>
 </head>
-
-<body>
-    
-<div class="loading" id="loading"></div>
-
-<header>
-<div class="header" id="header">
-    <div class="logo-group">
-        <span class="visuallyhidden">Federation</span>
-        <a href="index">
-            <img src="img/logo_reverse.png" alt="" class="logo">
-        </a>
-        <div class="mobile-nav-btn-group">
-            <div class="open-mobile-nav is-visible" id="open-mobile-nav">
-                <span class="visuallyhidden">open menu</span>
-            </div>
-            <div class="close-mobile-nav" id="close-mobile-nav">
-                <span class="visuallyhidden">close menu</span>
-            </div>
-        </div>
-    </div>
-    <nav>
-        <ul class="mobile-nav" id="mobile-nav">
-            <li class="mobile-nav-item"><a href="about" class="mobile-nav-link">About us</a></li>
-            <li class="mobile-nav-item"><a href="what-we-do" class="mobile-nav-link">What we do</a></li>
-			<li class="mobile-nav-item"><a href="our-work" class="mobile-nav-link">Our work</a></li>
-			<li class="mobile-nav-item"><a href="the-lab" class="mobile-nav-link">The lab</a></li>
-            <li class="mobile-nav-item"><a href="news" class="mobile-nav-link">News</a></li>
-            <li class="mobile-nav-item"><a href="lets-talk" class="mobile-nav-link">Let's talk</a></li>
-        </ul>
-        <ul class="main-nav">
-            <li class="main-nav-item"><a href="about" class="main-nav-link">About us</a></li>
-            <li class="main-nav-item"><a href="what-we-do" class="main-nav-link">What we do</a></li>
-			<li class="main-nav-item"><a href="our-work" class="main-nav-link">Our work</a></li>
-			<li class="main-nav-item"><a href="the-lab" class="main-nav-link">The lab</a></li>
-            <li class="main-nav-item"><a href="news" class="main-nav-link">News</a></li>
-            <li class="main-nav-item"><a href="lets-talk" class="main-nav-link">Let's talk</a></li>
-        </ul>
-    </nav>
-    <div class="mobile-nav-bg" id="mobile-nav-bg"></div>
-</div>
-</header>
+<body>	
+	<div class="loading" id="loading"></div>
+	<header>
+		<div class="header" id="header">
+			<div class="logo-group">
+				<span class="visuallyhidden">Federation</span>
+				<a href="index">
+					<img src="img/logo_reverse.png" alt="" class="logo">
+				</a>
+				<div class="mobile-nav-btn-group">
+					<div class="open-mobile-nav is-visible" id="open-mobile-nav">
+						<span class="visuallyhidden">open menu</span>
+					</div>
+					<div class="close-mobile-nav" id="close-mobile-nav">
+						<span class="visuallyhidden">close menu</span>
+					</div>
+				</div>
+			</div>
+			<nav>
+				<ul class="mobile-nav" id="mobile-nav">
+					<li class="mobile-nav-item"><a href="about" class="mobile-nav-link">About us</a></li>
+					<li class="mobile-nav-item"><a href="what-we-do" class="mobile-nav-link">What we do</a></li>
+					<li class="mobile-nav-item"><a href="our-work" class="mobile-nav-link">Our work</a></li>
+					<li class="mobile-nav-item"><a href="the-lab" class="mobile-nav-link">The lab</a></li>
+					<li class="mobile-nav-item"><a href="news" class="mobile-nav-link">News</a></li>
+					<li class="mobile-nav-item"><a href="lets-talk" class="mobile-nav-link">Let's talk</a></li>
+				</ul>
+				<ul class="main-nav">
+					<li class="main-nav-item"><a href="about" class="main-nav-link">About us</a></li>
+					<li class="main-nav-item"><a href="what-we-do" class="main-nav-link">What we do</a></li>
+					<li class="main-nav-item"><a href="our-work" class="main-nav-link">Our work</a></li>
+					<li class="main-nav-item"><a href="the-lab" class="main-nav-link">The lab</a></li>
+					<li class="main-nav-item"><a href="news" class="main-nav-link">News</a></li>
+					<li class="main-nav-item"><a href="lets-talk" class="main-nav-link">Let's talk</a></li>
+				</ul>
+			</nav>
+			<div class="mobile-nav-bg" id="mobile-nav-bg"></div>
+		</div>
+	</header>
