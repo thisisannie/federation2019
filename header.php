f9a386bbe32d2f67ca68961f7dc74def73a93d86
Annie Kyles <anniek@federation.net.nz>
2022-05-25 09:51:56 +1200

    Removed site.manifest

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
 	<title>Federation | Leading change</title>
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="manifest" href="site.webmanifest">
 	<link rel="shortcut icon" type="image/x-icon" href="/favicon.png"/>   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500&family=Raleway:wght@100;300;400;500;700&display=swap" rel="stylesheet"> 
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/bootstrap-grid.css">
