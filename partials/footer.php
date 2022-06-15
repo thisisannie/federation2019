360845658a7abdc031e64cd072f660a391e50695
Annie Kyles <anniek@federation.net.nz>
2022-06-15 16:00:48 +1200

    Deferred innessential assets

--- partials/footer.php
+++ partials/footer.php
@@ -1,27 +1,26 @@
 <footer>
 <div class="footer">
-    <a href="<?=HTML_ROOT;?>"><img src="<?=HTML_ROOT;?>/img/logo.png" alt="" class="footer__logo"></a>
+    <a href="<?=HTML_ROOT;?>"><img defer src="<?=HTML_ROOT;?>/img/logo.png" alt="" class="footer__logo"></a>
     <a href="<?=HTML_ROOT;?>/lets-talk" class="footer__link-button">Let’s talk&nbsp;<span>about your goals</span></a>
-    <a href="https://www.linkedin.com/company/federation./?originalSubdomain=nz" target="_blank"><img src="<?=HTML_ROOT;?>/img/linkedin.png" alt="" class="footer__linkedin"></a>
+    <a href="https://www.linkedin.com/company/federation./?originalSubdomain=nz" target="_blank"><img defer src="<?=HTML_ROOT;?>/img/linkedin.png" alt="" class="footer__linkedin"></a>
 </div>
 </footer>
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="<?=HTML_ROOT;?>/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+<script defer src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
-<script type="text/javascript" src="<?=HTML_ROOT;?>/js/slick.min.js"></script>
+<script defer type="text/javascript" src="<?=HTML_ROOT;?>/js/slick.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/jquery.gsap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/animation.gsap.min.js"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min.js"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/jquery.gsap.min.js"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/animation.gsap.min.js"></script>
 
-<script src="https://www.youtube.com/player_api"></script>
-<script src="<?=HTML_ROOT;?>/js/video-manager.js?v2"></script>
-<script src="<?=HTML_ROOT;?>/js/animation.js"></script>
-<script src="<?=HTML_ROOT;?>/js/main.js"></script>
+<script defer src="https://www.youtube.com/player_api"></script>
+<script defer src="<?=HTML_ROOT;?>/js/video-manager.js?v2"></script>
+<script defer src="<?=HTML_ROOT;?>/js/animation.js"></script>
+<script defer src="<?=HTML_ROOT;?>/js/main.js"></script>
 
-<script>
+<script defer>
 //load video manager after YouTube API has loaded
 function onYouTubeIframeAPIReady() {
   VideoManager.init();
