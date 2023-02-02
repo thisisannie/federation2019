323a23e32bcb6687b37c6c131785bccf54d49f09
Annie Kyles <anniek@federation.net.nz>
2023-02-02 14:02:42 +1300

    Climate Sustainability copy tweaks.

--- news/climate.php
+++ news/climate.php
@@ -1,6 +1,6 @@
 <?php 
 	$page_title = "Massey University";
-	$page_description = "Federation Wins Massey University Account After 4-Way Pitch for brand, integrated campaigns and media.";
+	$page_description = "The first agency in Aotearoa to be accredited in Business and Climate Change: Towards Net Zero Transmission from the University of Cambridge Institute for Sustainability Leadership";
 
 	include '../config.php';
 	include ABSPATH . '/partials/header.php';
@@ -10,7 +10,7 @@
 		"title1" => "Sustainability",
 		"title2" => "Consulting",
 		"client" => "1 February 2022",
-		"intro" => "Over the last year we’ve been busy applying our learnings and helping organisations and marketers on their journey to net zero.  Here are some highlights.",
+		"intro" => "Hard to believe it’s been 12 months since we became the first agency in Aotearoa to be accredited in Business and Climate Change: Towards Net Zero Transmission from the University of Cambridge Institute for Sustainability Leadership. Since then we’ve been busy applying our learnings!",
 		"slug" => "climate",
 		"bannerimg" => "ebus_motion_1400x600.webp",
 	];
@@ -50,10 +50,8 @@
 			<div class="row justify-content-center">
 				<div class="col-12 col-sm-8 col-lg-7 col-xl-7">
 					<div class="project-detail__description">
-						<p class="animate animate--fade-up-25"><strong>Hard to believe it’s been 12 months since Federation became the first Agency in Aotearoa to be accredited in Business and Climate Change: Towards Net Zero Transmission from the University of Cambridge Institute for Sustainability Leadership.</strong></p>
-
-						<p class="animate animate--fade-up-25"><a href="https://campaignbrief.co.nz/2022/04/21/federation-becomes-first-creative-agency-in-aotearoa-to-launch-sustainability-consulting/" target="_blank">Read Campaign brief Article: Federation becomes first creative agency in Aotearoa to launch sustainability consulting</a></p>
-						<p class="animate animate--fade-up-25">Over the last year we’ve been busy applying our learnings and helping organisations and marketers on their journey to net zero.  Here are some highlights.</p>
+					<p class="animate animate--fade-up-25"><a href="https://campaignbrief.co.nz/2022/04/21/federation-becomes-first-creative-agency-in-aotearoa-to-launch-sustainability-consulting/" target="_blank">Read Campaign brief Article: Federation becomes first creative agency in Aotearoa to launch sustainability consulting</a></p>
+						<p class="animate animate--fade-up-25">Over the last year we’ve helped a range of organisations and marketers on their journey to net zero. Here are some highlights.</p>
 						<p class="animate animate--fade-up-25">Back in May 2022 we held a thought-provoking session to kick-start the return of the Marketing Association’s CMO breakfast where our lead team spoke about how authentic sustainability and purpose can be embedded into brands to create a nucleus for future growth. We were privileged to have our clients LOréal and Auckland Transport talk about their transition to Net Zero and it was fascinating to hear the experience so many senior marketers are having in their industries in their own journeys.</p>
 						<div class="feature-video feature animate animate--fade-up-25" id="video-<?php echo $array['slug']; ?>" style="margin-bottom:1.5em;">
 								<div class="feature-video__image-group">
